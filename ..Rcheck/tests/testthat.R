45ee747376451084d9ea6a93171fcb990cc0f2e2
cjb873 <cjb873@nau.edu>
2023-10-20 14:11:37 -0700

    Remove testing info

--- ..Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(BeckRPackage)
-
-test_check("BeckRPackage")
--- ..Rcheck/tests/testthat/test_kmeans_interface.R
+++
@@ -1,4 +0,0 @@
-test_that("returned vector is long enough", {
-  clustering <- kmeans_interface(3, as.matrix(iris[3:4]))
-  expect_equal(length(clustering), nrow(as.matrix(iris[3:4])))
-  })
